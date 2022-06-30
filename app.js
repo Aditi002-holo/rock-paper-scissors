2bde562f3055fd65a99026f3ba0e7aa6447f4bc2
Aditi002-holo <aditideshpande2908@gmail.com>
2022-06-30 16:24:35 +0530

    add playRound() - plays a round of the game

--- app.js
+++ app.js
@@ -7,3 +7,44 @@ function computerPlay() {
     return choice[Math.floor(Math.random() * choice.length)];
 }
 
+// a single round of rock, paper, scissors
+function playRound(playerSelection, computerSelection) {
+    const playerChoice = playerSelection.toLowerCase();
+    console.log(playerChoice, computerSelection);
+    if(playerChoice === 'rock') {
+        if(computerSelection === 'rock') {
+            return `It's ${playerChoice} Vs. ${computerSelection}. Play Again!`;
+        } 
+        else if(computerSelection === 'paper') {
+            return `You Lose! ${computerSelection} beats ${playerChoice}`;
+        } 
+        else if(computerSelection === 'scissors') {
+            return `You Win! ${playerChoice} beats ${computerSelection}`;
+        }
+    }
+
+    if(playerChoice === 'paper') {
+        if(computerSelection === 'rock') {
+            return `You Win! ${playerChoice} beats ${computerSelection}`;       
+        } 
+        else if(computerSelection === 'paper') {
+            return `It's ${playerChoice} Vs. ${computerSelection}. Play Again!`; 
+        } 
+        else if(computerSelection === 'scissors') {
+            return `You Lose! ${computerSelection} beats ${playerChoice}`;
+        }
+    }
+
+    if(playerChoice === 'scissors') {
+        if(computerSelection === 'rock') {
+            return `You Lose! ${computerSelection} beats ${playerChoice}`;
+        } 
+        else if(computerSelection === 'paper') {
+            return `You Win! ${playerChoice} beats ${computerSelection}`; 
+        } 
+        else if(computerSelection === 'scissors') {
+            return `It's ${playerChoice} Vs. ${computerSelection}. Play Again!`;
+        }
+    }
+}
+
